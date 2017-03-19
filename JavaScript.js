8b07f504b3f4a9e16b5a924c34e66ca39d346069
MrSirius <mrsirius@rambler.ru>
2017-03-19 19:55:56 +0300

    Пустой файл JS

---
+++ JavaScript.js
@@ -0,0 +1,4 @@
+var namePersonElement = document.getElementById('namePerson');
+console.log(namePersonElement.innerHTML);
+var newnamePersonElement = prompt('Введите ваше имя');
+namePersonElement.innerHTML = newnamePersonElement;
